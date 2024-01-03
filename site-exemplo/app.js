f502287563e603adc7b5a2dfe9ae80bb2e53eb0b
viniciusabr <viniciusabreu2001@gmail.com>
2024-01-03 15:08:49 -0300

    mudanças

--- site-exemplo/app.js
+++
@@ -1,5 +0,0 @@
-function testar(){
-    var nome = document.getElementById('txt-nome').value;
-    var resultado = document.getElementById('js')
-    resultado.innerHTML = 'OLÁ ' + nome.toUpperCase() + '!' + ' TUDO BEM? VOCÊ PASSOU POR TESTES E CONSTATAMOS QUE VOCÊ É VIADO &#128517;🦌'
-}
---
+++ site-exemplo/script.js
@@ -0,0 +1,10 @@
+function entrar(){
+    var vermelho = document.getElementById('vermelho')
+    var preto = document.getElementById('preto')
+    vermelho.style.backgroundColor = 'green'
+    preto.style.backgroundColor = 'yellow'
+
+}
+function sair(){
+
+}
