b9f618706cf683d472d7fdad24d8245f3df8818d
viniciusabr <viniciusabreu2001@gmail.com>
2024-01-05 14:31:31 -0300

    testando

--- site-exemplo/script.js
+++ site-exemplo/script.js
@@ -1,10 +1,54 @@
-function entrar(){
-    var vermelho = document.getElementById('vermelho')
-    var preto = document.getElementById('preto')
-    vermelho.style.backgroundColor = 'green'
-    preto.style.backgroundColor = 'yellow'
+"use strict";
 
-}
-function sair(){
+let horas = 0;
+let minutos = 0;
+let segundos = 0;
+let milisegundos = 0;
 
-}
+let cron;
+
+document.form_main.start.onclick = () => start();
+document.form_main.pause.onclick = () => pause();
+document.form_main.reset.onclick = () => reset();
+
+function start() {
+    pause();
+    cron = setInterval(() => { timer(); }, 10);
+  }
+  
+  function pause() {
+    clearInterval(cron);
+  }
+  
+  function reset() {
+    horas = 0;
+    minutos = 0;
+    segundos = 0;
+    milisegundos = 0;
+    document.getElementById('horas').innerText = '00';
+    document.getElementById('minutos').innerText = '00';
+    document.getElementById('segundos').innerText = '00';
+    document.getElementById('milisegundos').innerText = '000';
+  }
+  function timer() {
+    if ((milisegundos += 10) == 1000) {
+      milisegundos = 0;
+      segundos++;
+    }
+    if (segundos == 60) {
+      segundos = 0;
+      minutos++;
+    }
+    if (minutos == 60) {
+      minutos = 0;
+      horas++;
+    }
+    document.getElementById('horas').innerText = returnData(horas);
+    document.getElementById('minutos').innerText = returnData(minutos);
+    document.getElementById('segundos').innerText = returnData(segundos);
+    document.getElementById('milisegundos').innerText = returnData(milisegundos);
+  }
+  
+  function returnData(input) {
+    return input > 10 ? input : `0${input}`
+  }
